aa21d7fbccd4c33d8eb9421c2501e6a1945cb8b0
pms8065 <90294662+pms8065@users.noreply.github.com>
2021-12-10 14:54:11 +0900

    Modify Testimonials

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ document.addEventListener('scroll' , () => {
 );
 
 
-
 // Handle scrolling when tapping on the navbar menu //
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -33,17 +32,20 @@ navbarMenu.addEventListener('click', (event) => {
 
 });
 
+
 // Navbar toggle button for small screen //
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', ()=> {
     navbarMenu.classList.toggle('open');
-})
+});
+
 
 // Handle click on "contactme" button on home //
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 });
+
 
 // Make home slowly fade to transparent as the window scrolls down //
 const home = document.querySelector('.home__container');
@@ -52,6 +54,7 @@ document.addEventListener('scroll', ()=> {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 
 });
+
 
 // Show "arrow up" button when scrolling down //
 const arrowUp = document.querySelector('.arrow-up');
@@ -64,10 +67,12 @@ document.addEventListener('scroll', () => {
 
 });
 
+
 // Handle click on the "arrow up" button //
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
-})
+});
+
 
 // Projects //
 const workBtnContainer = document.querySelector('.work__categories');
